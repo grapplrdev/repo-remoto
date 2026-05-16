9cf70e5dd327419f90e11eb89a4f19bc00fb0c80
glapplrdev <panquesitolyydani@gmail.com>
2026-05-16 16:56:39 -0500

    Corrección del código de mi prima

--- holaGit.java
+++ holaGit.java
@@ -1,10 +1,10 @@
-import.java.util.Scanner;
+import java.util.Scanner;
 public class holaGit {
   public static void main(String[] args) {
-    Scanner lector new  Scanner(System.in);
+    Scanner lector = new  Scanner(System.in);
     String nombre;
     System.out.println("Ingresa tu nombre");
-    nombre = Scanner lector Netxline();
+    nombre = lector.nextLine();
     
     System.out.println("Hola "+nombre);
   }
